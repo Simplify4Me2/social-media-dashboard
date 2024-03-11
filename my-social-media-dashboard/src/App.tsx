5b6b740ce84fc5a20212648c8cd48cb953364371
Simon <simon.vereecke@live.com>
2024-03-11 21:44:11 +0100

    setup tailwind copy

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -3,14 +3,18 @@ import "./App.css";
 function App() {
   return (
     <>
-      {/* <input type="checkbox"></input> */}
-    <div className="flex items-center">
-      <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
-      <label className="switch">
-        <input type="checkbox" />
-        <span className="slider round"></span>
-      </label>
-    </div>
+      <div className="flex items-center m-5">
+        <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
+        <input type="checkbox"></input>
+      </div>
+
+      <div className="flex items-center m-5">
+        <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+      </div>
     </>
   );
 }
