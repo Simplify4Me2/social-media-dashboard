28ef61c3e7d296035278389fff6be14b59da0ce4
Simplify4Me2 <simon.vereecke@live.com>
2024-03-14 21:26:08 +0100

    translate css to tailwind

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -5,7 +5,10 @@ function App() {
     <>
       <div className="flex items-center m-5">
         <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
-        <input type="checkbox"></input>
+        <label className="relative inline-block w-16 h-8">
+          <input type="checkbox" className="opacity-0 h-0 w-0"></input>
+          <span className="absolute cursor-pointer bg-gray-300 rounded-3xl before:rounded-3xl top-0 left-0 right-0 bottom-0"></span>
+        </label>
       </div>
 
       <div className="flex items-center m-5">
