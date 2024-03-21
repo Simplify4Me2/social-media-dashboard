61e00336bc556666a353c790034c3077114d86fb
Simon <simon.vereecke@live.com>
2024-03-21 20:52:05 +0100

    tailwind copy

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -5,9 +5,9 @@ function App() {
     <>
       <div className="flex items-center m-5">
         <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
-        <label className="relative inline-block w-16 h-8">
+        <label className="relative inline-block w-[60px] h-[34px]">
           <input type="checkbox" className="opacity-0 h-0 w-0"></input>
-          <span className="absolute cursor-pointer bg-gray-300 rounded-3xl before:rounded-3xl top-0 left-0 right-0 bottom-0"></span>
+          <span className="absolute cursor-pointer gray rounded-3xl before:rounded-3xl top-0 left-0 right-0 bottom-0 transition slider"></span>
         </label>
       </div>
 
