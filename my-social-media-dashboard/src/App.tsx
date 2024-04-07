efecab6b112dcadbcd61acf173dca415db37ca6a
Simon <simon.vereecke@live.com>
2024-04-07 20:30:42 +0200

    add icons for card

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -4,9 +4,20 @@ import { ToggleSwitch } from "./components";
 function App() {
   return (
     <>
-      <div className="flex items-center m-5">
-        <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
-        <ToggleSwitch />
+      <Header />
+      <div className="flex flex-col max-w-fit card-bg p-5">
+        <div className="flex flew-row ">
+          <img src="icon-facebook.svg" className="w-5 h-5 mr-1" />
+          <span>@nathanf</span>
+        </div>
+        <div className="flex flex-col m-3">
+          <span>1987</span>
+          <span>FOLLOWERS</span>
+        </div>
+        <div className="flex flew-row items-center">
+          <img src="icon-up.svg" className="w-2 h-1 mr-1" />
+          <span className="lime">12 Today</span>
+        </div>
       </div>
     </>
   );
@@ -14,4 +25,17 @@ function App() {
 
 export default App;
 
-
+function Header() {
+  return (
+    <header className="flex justify-between w-screen">
+      <div>
+        <h1>Social Media Dashboard</h1>
+        <h3>Total Followers: 23,004</h3>
+      </div>
+      <div>
+        <span className="mr-3 font-bold text-slate-400">Dark Mode</span>
+        <ToggleSwitch />
+      </div>
+    </header>
+  );
+}
