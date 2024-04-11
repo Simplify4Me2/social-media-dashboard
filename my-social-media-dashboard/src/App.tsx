7a36ab8d3e34895c4384680f70d51c9a0305b329
Simon <simon.vereecke@live.com>
2024-04-11 19:15:34 +0200

    add Inter font

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -5,20 +5,7 @@ function App() {
   return (
     <>
       <Header />
-      <div className="flex flex-col max-w-fit card-bg p-5">
-        <div className="flex flew-row ">
-          <img src="icon-facebook.svg" className="w-5 h-5 mr-1" />
-          <span>@nathanf</span>
-        </div>
-        <div className="flex flex-col m-3">
-          <span>1987</span>
-          <span>FOLLOWERS</span>
-        </div>
-        <div className="flex flew-row items-center">
-          <img src="icon-up.svg" className="w-2 h-1 mr-1" />
-          <span className="lime">12 Today</span>
-        </div>
-      </div>
+      <Card />
     </>
   );
 }
@@ -27,9 +14,9 @@ export default App;
 
 function Header() {
   return (
-    <header className="flex justify-between w-screen">
+    <header className="flex justify-between w-full font-bold">
       <div>
-        <h1>Social Media Dashboard</h1>
+        <h1 className="text-3xl">Social Media Dashboard</h1>
         <h3>Total Followers: 23,004</h3>
       </div>
       <div>
@@ -37,5 +24,24 @@ function Header() {
         <ToggleSwitch />
       </div>
     </header>
+  );
+}
+
+function Card() {
+  return (
+    <div className="flex flex-col max-w-fit card-bg p-5">
+      <div className="flex flew-row ">
+        <img src="icon-facebook.svg" className="w-5 h-5 mr-1" />
+        <span>@nathanf</span>
+      </div>
+      <div className="flex flex-col m-3">
+        <span>1987</span>
+        <span>FOLLOWERS</span>
+      </div>
+      <div className="flex flew-row items-center">
+        <img src="icon-up.svg" className="w-2 h-1 mr-1" />
+        <span className="lime">12 Today</span>
+      </div>
+    </div>
   );
 }
--- my-social-media-dashboard/tailwind.config.js
+++ my-social-media-dashboard/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Inter', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        '3xl': '1.75rem'
+      }
+    },
   },
   plugins: [],
 }
