628998b8e7d1ebb44552362bf75115b5f3b03dbb
Simon <simon.vereecke@live.com>
2024-04-13 22:44:12 +0200

    multiple cards

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -1,27 +1,43 @@
 import "./App.css";
+import { Card } from "./card";
 import { Header } from "./header";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="my-11 mx-0.5">
-        {/* <Card /> */}
-        <div className="w-64 h-56 flex flex-col-reverse facebook rounded">
-          <div className="bg-card w-64 h-55 rounded-b-sm flex flex-col justify-center">
-            <div className="flex flew-row items-center justify-center">
-              <img src="icon-facebook.svg" className="w-5 h-5 mr-2" />
-              <span className="text-xs font-bold">@nathanf</span>
-            </div>
-            <div className="flex flex-col m-3">
-              <span className="text-6xl font-bold">1987</span>
-              <span className="text-xs tracking-widest">FOLLOWERS</span>
-            </div>
-            <div className="flex flew-row items-center justify-center">
-              <img src="icon-up.svg" className="w-2 h-1 mr-1" />
-              <span className="text-xs lime font-bold">12 Today</span>
-            </div>
-          </div>
+      <div className="my-11 mx-0.5 flex justify-between">
+        <div>
+          <Card
+            socialMediaProvider="facebook"
+            followersCount={1987}
+            followersCountToday={12}
+            handleName="@nathanf"
+          />
+        </div>
+        <div>
+          <Card
+            socialMediaProvider="twitter"
+            followersCount={1044}
+            followersCountToday={99}
+            handleName="@nathanf"
+          />
+        </div>
+        <div>
+          <Card
+            socialMediaProvider="instagram"
+            followersCount={11000}
+            followersCountToday={1099}
+            handleName="@realnathanf"
+          />
+        </div>
+        <div>
+          <Card
+            socialMediaProvider="youtube"
+            followersCount={8239}
+            followersCountToday={-144}
+            handleName="Nathan F."
+          />
         </div>
       </div>
     </>
@@ -30,21 +46,4 @@ function App() {
 
 export default App;
 
-function Card() {
-  return (
-    <div className="flex flex-col max-w-fit card-bg p-11">
-      <div className="flex flew-row ">
-        <img src="icon-facebook.svg" className="w-5 h-5 mr-1" />
-        <span>@nathanf</span>
-      </div>
-      <div className="flex flex-col m-3">
-        <span>1987</span>
-        <span>FOLLOWERS</span>
-      </div>
-      <div className="flex flew-row items-center">
-        <img src="icon-up.svg" className="w-2 h-1 mr-1" />
-        <span className="lime">12 Today</span>
-      </div>
-    </div>
-  );
-}
+
---
+++ my-social-media-dashboard/src/card.tsx
@@ -0,0 +1,42 @@
+interface ICardProps {
+    socialMediaProvider: "facebook" | "instagram" | "twitter" | "youtube";
+    handleName: string;
+    followersCount: number;
+    followersCountToday: number;
+  }
+  
+  export function Card({ followersCount, followersCountToday, handleName, socialMediaProvider }: ICardProps) {
+
+    function getFollowersCount() {
+        if (followersCount < 10_000)
+            return followersCount;
+
+        const thousands = followersCount / 1000;
+
+        return `${thousands}k`;
+    }
+
+    return (
+      <div
+        className={`w-64 h-56 flex flex-col-reverse ${socialMediaProvider} rounded`}
+      >
+        <div className="bg-card w-64 h-55 rounded-b-sm flex flex-col justify-center">
+          <div className="flex flew-row items-center justify-center">
+            <img
+              src={`icon-${socialMediaProvider}.svg`}
+              className="w-5 h-5 mr-2"
+            />
+            <span className="text-xs font-bold">{handleName}</span>
+          </div>
+          <div className="flex flex-col m-3">
+            <span className="text-6xl font-bold">{getFollowersCount()}</span>
+            <span className="text-xs tracking-widest">FOLLOWERS</span>
+          </div>
+          <div className="flex flew-row items-center justify-center">
+            <img src="icon-up.svg" className="w-2 h-1 mr-1" />
+            <span className="text-xs lime font-bold">{followersCountToday} Today</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
