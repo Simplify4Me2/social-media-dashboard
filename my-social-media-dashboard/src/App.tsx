eaf412cd0f3b7abe0590072aa9b7a944ed1888cf
Simon <simon.vereecke@live.com>
2024-04-14 19:20:16 +0200

    remove div

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -7,38 +7,30 @@ function App() {
     <>
       <Header />
       <div className="my-11 mx-0.5 flex justify-between">
-        <div>
           <Card
             socialMediaProvider="facebook"
             followersCount={1987}
             followersCountToday={12}
             handleName="@nathanf"
           />
-        </div>
-        <div>
           <Card
             socialMediaProvider="twitter"
             followersCount={1044}
             followersCountToday={99}
             handleName="@nathanf"
           />
-        </div>
-        <div>
           <Card
             socialMediaProvider="instagram"
             followersCount={11000}
             followersCountToday={1099}
             handleName="@realnathanf"
           />
-        </div>
-        <div>
           <Card
             socialMediaProvider="youtube"
             followersCount={8239}
             followersCountToday={-144}
             handleName="Nathan F."
           />
-        </div>
       </div>
     </>
   );
