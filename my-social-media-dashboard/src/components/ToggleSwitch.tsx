292db7712e8f7cc1c9cc46d7f418143df40a93d0
Simon <simon.vereecke@live.com>
2024-07-25 19:53:37 +0200

    Form elements must have labels

--- my-social-media-dashboard/src/components/ToggleSwitch.tsx
+++ my-social-media-dashboard/src/components/ToggleSwitch.tsx
@@ -12,6 +12,7 @@ export function ToggleSwitch({ isEnabled, onChange }: IToggleSwitchProps) {
           checked={isEnabled}
           onChange={onChange}
           className="sr-only"
+          aria-label="Toggle dark mode"
         />
         <span className={`box block h-[24px] w-[48px] rounded-full bg-gradient-to-r ${isEnabled ? `from-true-blue to-green` : `bg-gray`} hover:from-true-blue hover:to-green`}>
         <div className={`relative left-[3px] top-[3px] flex h-[18px] w-[18px] items-center justify-center rounded-full bg-white dark:bg-dark-blue-600 
