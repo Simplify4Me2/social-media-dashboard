a5697e19acf8fccd0de26c241da94c2f89d76c05
Simon <simon.vereecke@live.com>
2024-07-25 15:18:34 +0200

    update toggle button color to almost invisible background

--- my-social-media-dashboard/src/components/ToggleSwitch.tsx
+++ my-social-media-dashboard/src/components/ToggleSwitch.tsx
@@ -14,7 +14,7 @@ export function ToggleSwitch({ isEnabled, onChange }: IToggleSwitchProps) {
           className="sr-only"
         />
         <span className={`box block h-[24px] w-[48px] rounded-full bg-gradient-to-r ${isEnabled ? `from-true-blue to-green` : `bg-gray`} hover:from-true-blue hover:to-green`}>
-        <div className={`relative left-[3px] top-[3px] flex h-[18px] w-[18px] items-center justify-center rounded-full bg-white dark:bg-dark-blue-800 
+        <div className={`relative left-[3px] top-[3px] flex h-[18px] w-[18px] items-center justify-center rounded-full bg-white dark:bg-dark-blue-600 
           transition ${isEnabled ? "" : "translate-x-6"}`}></div>
         </span>
       </label>
