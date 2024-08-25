cffed3edf042c85c242fc3d697ead2d64f58fee0
Simon <simon.vereecke@live.com>
2024-08-25 08:09:58 +0200

    add static dark background

--- my-social-media-dashboard/src/App.tsx
+++ my-social-media-dashboard/src/App.tsx
@@ -108,7 +108,7 @@ function App() {
           isDarkModeEnabled ? "dark" : ""
         } dark:bg-dark-blue-800 flex flex-col`}
       >
-        {/* <div className="absolute bg-dark-blue-600 lg:h-64 w-full" /> */}
+        <div className="absolute dark:bg-dark-blue-600 lg:h-64 w-full z-0" />
         <Header>
           <span className="mr-3 mt-1 text-sm text-blue-700 dark:text-blue-400 lg:text-slate-400">
             Dark Mode
@@ -118,7 +118,7 @@ function App() {
             onChange={toggleDarkMode}
           />
         </Header>
-        <section className="my-4 mx-6 lg:mt-7 lg:w-276 lg:self-center">
+        <section className="my-4 mx-6 lg:mt-7 lg:w-276 lg:self-center z-10">
           <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {socialMediaData.map((item) => (
               <Card
@@ -149,112 +149,6 @@ function App() {
         </section>
       </main>
     </>
-  );
-}
-
-function Old() {
-  const { isEnabled: isDarkModeEnabled, toggle: toggleDarkMode } =
-    useDarkModeState();
-
-  return (
-    <main
-      className={`h-full lg:h-screen ${
-        isDarkModeEnabled ? "dark" : ""
-      } dark:bg-dark-blue-800`}
-    >
-      <div className="dark:bg-dark-blue-600 lg:h-64">
-        <div className="max-w-7xl p-[1.6rem] text-center my-0 mx-auto lg:px-[1.975rem]">
-          <Header>
-            <span className="mr-3 mt-1 text-sm text-blue-700 dark:text-blue-400 lg:text-slate-400">
-              Dark Mode
-            </span>
-            <ToggleSwitch
-              isEnabled={isDarkModeEnabled}
-              onChange={toggleDarkMode}
-            />
-          </Header>
-          <div className="mt-6 lg:mt-11 mb-11 lg:mb-5 mx-0.5 flex justify-between flex-wrap">
-            <Card
-              socialMediaProvider="facebook"
-              followersCount={1987}
-              followersCountToday={12}
-              handleName="@nathanf"
-            />
-            <Card
-              socialMediaProvider="twitter"
-              followersCount={1044}
-              followersCountToday={99}
-              handleName="@nathanf"
-            />
-            <Card
-              socialMediaProvider="instagram"
-              followersCount={11000}
-              followersCountToday={1099}
-              handleName="@realnathanf"
-            />
-            <Card
-              socialMediaProvider="youtube"
-              followersCount={8239}
-              followersCountToday={-144}
-              handleName="Nathan F."
-            />
-          </div>
-          <h2 className="text-left font-bold text-2xl leading-none text-blue-700 dark:text-blue-400 pt-1">
-            Overview - Today
-          </h2>
-          <div className="mt-6 my-11 mx-0.5 flex flex-wrap justify-between">
-            <OverviewCard
-              socialMediaProvider="facebook"
-              label="Page Views"
-              count={87}
-              percentage={3}
-            />
-            <OverviewCard
-              socialMediaProvider="facebook"
-              label="Likes"
-              count={52}
-              percentage={-2}
-            />
-            <OverviewCard
-              socialMediaProvider="instagram"
-              label="Likes"
-              count={5462}
-              percentage={2257}
-            />
-            <OverviewCard
-              socialMediaProvider="instagram"
-              label="Profile Views"
-              count={52000}
-              percentage={1375}
-            />
-            <OverviewCard
-              socialMediaProvider="twitter"
-              label="Retweets"
-              count={117}
-              percentage={303}
-            />
-            <OverviewCard
-              socialMediaProvider="twitter"
-              label="Likes"
-              count={507}
-              percentage={553}
-            />
-            <OverviewCard
-              socialMediaProvider="youtube"
-              label="Likes"
-              count={107}
-              percentage={-19}
-            />
-            <OverviewCard
-              socialMediaProvider="youtube"
-              label="Total Views"
-              count={1407}
-              percentage={-12}
-            />
-          </div>
-        </div>
-      </div>
-    </main>
   );
 }
 
